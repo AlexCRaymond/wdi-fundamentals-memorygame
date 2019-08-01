af33074720efdd421189a9d33a237afb9ca8bcdd
Alex Raymond <you@example.com>
2019-08-01 15:03:28 -0700

    created game logic3

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'images/queen-of-hearts.png',
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png',
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'images/king-of-hearts.png',
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: 'images/king-of-diamonds.png',
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch () {
@@ -8,13 +29,15 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 			}	
 		else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 		}
 			};
 
 function flipCard (cardID) {
-	console.log ("User flipped " + cards[cardID]);
-		cardsInPlay.push(cards[cardID]);
+	console.log ("User flipped " + cards[cardID].rank);
+		cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 		if (cardsInPlay.length === 2)  {
 			checkForMatch();			
 		}
